01bf6ef006a3d2fdb8b2a0386dd268206d3589b0
David R. Stewart <randy.stewart1166@gmail.com>
2021-03-25 09:02:33 -0600

    Add files via upload
    TMB file to estimate parameters of the von Bertalanffy growth model

---
+++ TMB_vonB.R
@@ -0,0 +1,73 @@
+#Load and install necessary packages
+install.packages("TMB")
+library(TMB)
+library(devtools)
+install_github("kaskr/TMB_contrib_R/TMBhelper")
+library(TMBhelper)
+
+#Use length at age data constructed from the age-length key in Box 7.2.
+#Load the lengthage.RData object from your working directory and save it as object la.new. This must first be created and saved using the code in Box 7.2. If the file was saved to a different working directory, either change the working directory to the file's location or add the directory path to the lengthage.RData file (e.g., "G:/AFS Book Chapter/lengthage.RData")
+setwd("G:/AFS Book Chapter")
+la.new<-get(load("G:/AFS Book Chapter/lengthage.RData"))
+#head(la.new)
+#Use View(la.new) to view the entire data set and how it should be entered in a spreadsheet
+
+#Write C++ model template
+#Note: Comments follow //
+tmb_model="
+// von Bertalanffy growth function
+#include <TMB.hpp>
+
+template<class Type>
+Type objective_function<Type>::operator() () {
+
+// DATA_SECTION
+  DATA_VECTOR(Age); //Vector of age data
+  DATA_VECTOR(Length); //Vector of length data
+  int n = Age.size(); //Number of individuals
+
+// PARAMETER_SECTION
+  PARAMETER(Linf); // Asymptoptic length
+  PARAMETER(k); // Curvature parameter
+  PARAMETER(t0); // Location parameter 
+  PARAMETER(logSigma); // Fit sigma on a log scale to keep it > 0
+
+// PRELIMINARY_CALCS_SECTION: (Transformed parameters)
+  Type sigma = exp(logSigma);
+  ADREPORT(sigma)
+
+// PROCEDURE_SECTION
+  vector<Type> La_i(n);
+  La_i = Linf*(1-exp(-k*(Age-t0))); //von Bertalanffy growth equation
+
+  Type nll = 0.0; // Initialize negative log-likelihood
+
+  for(int i = 0; i < n; i++){ // C++ starts loops at 0!
+  // negative log likelihood
+  nll -= dnorm(Length[i], La_i[i], sigma, TRUE); 
+}
+
+return nll;
+}"
+#Write model to C++
+write(tmb_model, file = "NLL_vbgf.cpp")
+
+#Load model
+compile("NLL_vbgf.cpp") #Compile into a shared object file
+
+dyn.load(dynlib("NLL_vbgf"))
+
+
+#Parameterize and run the model
+data<-list(Length=la.new$Length, Age=la.new$Age) #Define data to be tested
+parameters<-list(Linf=650,k=0.4,t0=0,logSigma=5) #Starting values
+obj = MakeADFun(data, parameters, DLL = "NLL_vbgf",silent=TRUE) #Prepare model objects for fitting
+vbgf_opt = nlminb(start=obj$par, obj=obj$fn, gr=obj$gr) #Fit model using nlminb optimizer (quasi-Newton method)
+(fit_vonB = summary(sdreport(obj))) #Summary of optimal parameter estimates and their Std. Errors
+
+#Plot observed and predicted data
+{with(la.new, plot(Length~Age, data=la.new,xlab= "Age (yr)",ylab="Total Length (mm)",las=1,bty="n",col="gray",xlim=c(0,max(la.new$Age)+1),ylim=c(0,max(la.new$Length)+100)))
+  curve(fit_vonB[1,1]*(1-exp(-fit_vonB[2,1]*(x-fit_vonB[3,1]))),from=1,to=max(la.new$Age),lwd=3,add=T)}
+
+
+
